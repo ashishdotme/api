8d05d96daff96976d31c2e6d84c82ba6cb0e4a48
Ashish Patel <ashishsushilpatel@gmail.com>
2024-09-12 18:05:31 +0000

    feat: add movies and shows endpoint

---
+++ build.ts
@@ -0,0 +1,67 @@
+import { ensureDir } from "https://deno.land/std@0.224.0/fs/ensure_dir.ts";
+
+
+const fetchMovies = async (): Promise<any[]> => {
+  const baseUrl = "https://api.ashish.me/movies";
+
+  console.log("Fetching movies");
+  const res = await fetch(`${baseUrl}`, {
+    method: "GET",
+  });
+
+  const json = await res.json();
+  return json
+};
+
+const fetchShows = async (): Promise<any[]> => {
+  const baseUrl = "https://api.ashish.me/shows";
+
+  console.log("Fetching Shows");
+  const res = await fetch(`${baseUrl}`, {
+    method: "GET",
+  });
+
+  const json = await res.json();
+  return json
+};
+
+const fetchBooks = async (): Promise<any[]> => {
+  const baseUrl = "https://api.ashish.me/books";
+
+  console.log("Fetching movies");
+  const res = await fetch(`${baseUrl}`, {
+    method: "GET",
+  });
+
+  const json = await res.json();
+  return json
+};
+
+await ensureDir("movies");
+await ensureDir("shows");
+await ensureDir("books");
+
+const movies = await fetchMovies();
+
+await Deno.writeTextFile(
+  "movies/all.json",
+  JSON.stringify(movies, null, 2) + "\n"
+);
+
+const shows = await fetchShows();
+
+await Deno.writeTextFile(
+  "shows/all.json",
+  JSON.stringify(shows, null, 2) + "\n"
+);
+
+const books = await fetchBooks();
+
+await Deno.writeTextFile(
+  "books/all.json",
+  JSON.stringify(books, null, 2) + "\n"
+);
+
+
+
+export {};
